a833d686eaa084bd36b098bbf9d62b376a0303b0
davidmerchan04 <100416357@alumnos.uc3m.es>
2020-03-09 11:11:38 +0100

    My app with data and first interactive plot

--- App/app.R
+++ App/app.R
@@ -8,43 +8,87 @@
 #
 
 library(shiny)
+library(httr)
+library(rlist)
+library(jsonlite)
+library(dplyr)
 
+
+###### The data is get from datos.gob.es
+### We get de API dato using de JSON format
+
+resp=GET("http://datos.gob.es/apidata/catalog/dataset/l01280066-transporte-publico-autobus-historico1.json")
+
+http_type(resp)
+http_error(resp)
+
+jsonRespText<-content(resp,as="text") 
+jsonRespText
+
+jsonRespParsed<-content(resp,as="parsed") 
+
+url=jsonRespParsed$result$items[[1]]$distribution[[4]]$accessURL
+
+######
+
+data=read.csv(url)
+head(data)
+View(data)
+
+jsonRespParsed
+
+names(data)
+
+names(data)[1]="Linea"
+names(data)[2]="Año"
+names(data)[4]="Número.anual.de.pasajeros"
+names(data)[5]="Expediciones.por.dia.laborable"
+names(data)[6]="Viajeros.por.dia"
+names(data)[7]="Viajeros.por.expedicion"
+names(data)[8]="Kilometros.anuales.realizados" 
+
+data$Tipo.de.transporte=gsub("Ãº","u",data$Tipo.de.transporte)
+
+dataPanel = tabPanel("Data",tableOutput("dataTable"),downloadButton("report", "Generate report"))
+
+histPanel = tabPanel("Histograms",
+                     fluidPage( 
+                         sidebarLayout(
+                             sidebarPanel(
+                                 sliderInput("bins",
+                                             "Number of bins:",
+                                             min = 1,
+                                             max = 50,
+                                             value = 30)
+                             ), # sidebarPanel
+                             mainPanel(
+                                 plotOutput("distPlot")
+                             ) # mainPanel
+                         ) # sidebarLayout
+                     ) # fluidPage
+)
 
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- navbarPage("Shiny App", dataPanel, histPanel)
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+    
+    output$dataTable = renderTable({
+        head(data)
+    })
+    
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
+        Viajeros.por.dia    <- faithful[, 2]
+        bins <- seq(min(Viajeros.por.dia), max(Viajeros.por.dia), length.out = input$bins + 1)
+        
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        hist(Viajeros.por.dia, breaks = bins, col = 'darkgray', border = 'white')
     })
+    
 }
 
 # Run the application 
